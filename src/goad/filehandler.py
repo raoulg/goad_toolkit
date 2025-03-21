7f96334f9faa1bcbbe261dfaebdf7ae94cb858b1
raoul grouls <Raoul.Grouls@han.nl>
2025-03-21 16:29:07 +0100

    filehandler

--- src/goad/filehandler.py
+++ src/goad/filehandler.py
@@ -1,16 +1,22 @@
+from pathlib import Path
+from typing import Optional
+
+import pandas as pd
 import requests
 from loguru import logger
 
-from goad.config import DataConfig
+from goad.config import FileConfig
 
 
 class FileHandler:
-    def __init__(self, config: DataConfig):
+    def __init__(self, config: FileConfig):
         self.config = config
         self.check_dirs()
 
-    def download(self) -> None:
-        filepath = self.config.data_dir / "raw" / self.config.filename
+    def download(self, filename: Optional[Path] = None) -> None:
+        if not filename:
+            filename = self.config.filename
+        filepath = self.config.data_dir / "raw" / filename
         if not filepath.exists():
             req = requests.get(self.config.url)
             with filepath.open("wb") as f:
@@ -31,15 +37,29 @@ class FileHandler:
                 logger.info(f"Creating directory {dir}")
                 dir.mkdir(parents=True, exist_ok=True)
 
-    def load(self) -> pd.DataFrame:
+    def load(self, filename: Optional[Path] = None, raw: bool = True) -> pd.DataFrame:
         """Load data from raw directory"""
-        raw_path = self.config.data_dir / "raw" / self.config.filename
-        data = pd.read_csv(raw_path)
-        logger.info(f"Loaded data from {raw_path}")
+        if raw:
+            subdir = "raw"
+        else:
+            subdir = "processed"
+
+        if not filename:
+            filename = self.config.filename
+
+        file_path = self.config.data_dir / subdir / filename
+        if not file_path.exists() and raw:
+            self.download(filename=filename)
+
+        data = pd.read_csv(file_path, parse_dates=["date"], index_col="date")
+        logger.info(f"Loaded data from {file_path}")
         return data
 
-    def save(self, data: pd.DataFrame) -> None:
+    def save(self, data: pd.DataFrame, filename: Optional[Path]) -> None:
         """Save data to processed directory"""
-        processed_path = self.config.data_dir / "processed" / self.config.filename
+        if not filename:
+            filename = self.config.filename
+
+        processed_path = self.config.data_dir / "processed" / filename
         data.to_csv(processed_path)
         logger.success(f"Saved processed data to {processed_path}")
