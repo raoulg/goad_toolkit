c6630bb3ef973371c69cdf1d09dd4c991d08251d
raoul grouls <Raoul.Grouls@han.nl>
2025-03-21 16:29:25 +0100

    split config

--- src/goad/config.py
+++ src/goad/config.py
@@ -2,12 +2,15 @@ from pathlib import Path
 
 from pydantic import BaseModel
 
-
-class DataConfig(BaseModel):
+class FileConfig(BaseModel):
     data_dir: Path = Path.home() / ".cache/mads_datasets/covid"
     filename: Path = Path("covid.csv")
-    start_date: str = "2020-10-01"
-    end_date: str = "2021-06-01"
     url: str = (
         "https://raw.githubusercontent.com/mzelst/covid-19/master/data/rivm_by_day.csv"
     )
+
+class DataConfig(BaseModel):
+    period: int = -14
+    window: int = 7
+    start_date: str = "2020-10-01"
+    end_date: str = "2021-06-01"
