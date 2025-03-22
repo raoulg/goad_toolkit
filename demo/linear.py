605bd089c6abfc4c65a9690553a9b3476e37103f
raoul grouls <Raoul.Grouls@han.nl>
2025-03-22 18:14:51 +0100

    demo

--- demo/linear.py
+++ demo/linear.py
@@ -2,8 +2,10 @@ from goad.config import DataConfig, FileConfig
 from goad.dataprocessor import CovidDataProcessor
 from goad.models import linear_model, mse, train_model
 from loguru import logger
-from goad.visualizer import ComparePlot, PlotSettings
+from goad.visualizer import ComparePlot, PlotSettings, ResidualPlot
 from pathlib import Path
+import pandas as pd
+import numpy as np
 
 
 def preprocess():
@@ -11,8 +13,6 @@ def preprocess():
     data_config = DataConfig()
     file_config = FileConfig()
 
-    # file_handler = FileHandler(file_config)
-    # raw = file_handler.load()
     covidprocessor = CovidDataProcessor(file_config, data_config)
     processed = covidprocessor.process()
     logger.success("Data processed.")
@@ -26,6 +26,13 @@ def check_path(filepath: Path):
         logger.info(f"Created directory {parentdir}")
 
 
+def save_fig(fig, imgpath):
+    assert fig is not None, "No figure was created"
+    check_path(Path(imgpath))
+    fig.savefig(imgpath)
+    logger.success(f"Z-Scores saved to {imgpath}")
+
+
 def viz_zscores(data):
     plotsettings = PlotSettings(
         xlabel="date",
@@ -33,46 +40,62 @@ def viz_zscores(data):
         title="Z-Scores of Deaths and Positive Tests",
     )
     compareplot = ComparePlot(plotsettings)
-    fig, ax = compareplot.plot(
-        data, x="date", y1="deaths_shifted_zscore", y2="positivetests_zscore"
+    fig, _ = compareplot.plot(
+        data=data, x="date", y1="deaths_shifted_zscore", y2="positivetests_zscore"
     )
-    assert fig is not None, "Scores are not created"
     imgpath = "data/result/zscores.png"
-    check_path(Path(imgpath))
-    fig.savefig(imgpath)
-    logger.success(f"Z-Scores saved to {imgpath}")
+    save_fig(fig, imgpath)
 
 
-def model(data):
+def model(data: "pd.DataFrame") -> "pd.DataFrame":
     X = data["positivetests"].values
     y = data["deaths"].values
+    assert isinstance(X, np.ndarray)
+    assert isinstance(y, np.ndarray)
+
     initial_params = [0.01, 1.0]
     bounds = [(0, 1.0), (0, None)]
     params = train_model(X, y, linear_model, mse, initial_params, bounds=bounds)
-    return params
-
-
-def viz_model(data, params):
-    x = data["positivetests"].values
-    yhat = linear_model(x, params)
+    logger.success(f"Fitted model with parameters: {params}")
+    yhat = linear_model(X, params)
     data["Predicted deaths"] = yhat
+    return data
+
+
+def viz_model(data):
     plotsettings = PlotSettings(xlabel="date", ylabel="deaths", title="Covid-19 data")
-    fig, ax = ComparePlot(plotsettings).plot(
-        data, x="date", y1="deaths_shifted", y2="Predicted deaths"
+    fig, _ = ComparePlot(plotsettings).plot(
+        data=data, x="date", y1="deaths_shifted", y2="Predicted deaths"
     )
 
-    assert fig is not None, "Model is not created"
     imgpath = "data/result/linear_results.png"
-    check_path(Path(imgpath))
-    fig.savefig(imgpath)
-    logger.success(f"Linear model results saved to {imgpath}")
+    save_fig(fig, imgpath)
+
+
+def viz_residual(data):
+    settings = PlotSettings(
+        figsize=(12, 6), title="Residual Plot", xlabel="dates", ylabel="error"
+    )
+    resplot = ResidualPlot(settings)
+    data["residual"] = data["deaths_shifted"].values - data["Predicted deaths"].values
+    fig, _ = resplot.plot(
+        data=data,
+        x="date",
+        y="residual",
+        date="2021-01-06",
+        datelabel="Vaccination Started",
+        interval=1,
+    )
+    imgpath = "data/result/residuals.png"
+    save_fig(fig, imgpath)
 
 
 def main():
     data = preprocess()
     viz_zscores(data)
-    params = model(data)
-    viz_model(data, params)
+    data = model(data)
+    viz_model(data)
+    viz_residual(data)
     logger.success("All done!")
 
 
