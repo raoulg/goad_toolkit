d70f904108bdc56321985bb66861378e51eec8c9
raoul grouls <Raoul.Grouls@han.nl>
2025-03-23 16:37:37 +0100

    add repr to config

--- src/goad_toolkit/config.py
+++ src/goad_toolkit/config.py
@@ -10,9 +10,15 @@ class FileConfig(BaseModel):
         "https://raw.githubusercontent.com/mzelst/covid-19/master/data/rivm_by_day.csv"
     )
 
+    def __repr__(self):
+        return f"FileConfig(data_dir={self.data_dir},\n filename={self.filename},\nurl={self.url})"
+
 
 class DataConfig(BaseModel):
     period: int = -14
     window: int = 7
     start_date: str = "2020-10-01"
     end_date: str = "2021-06-01"
+
+    def __repr__(self):
+        return f"DataConfig(period={self.period},\n window={self.window},\n start_date={self.start_date},\n end_date={self.end_date})"
